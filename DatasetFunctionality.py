ab9cbee8de12b78f95ac62b018c9ade6a3f28b32
Jonas Vervloet <42818980+JonasVervloet@users.noreply.github.com>
2019-11-29 20:15:00 +0100

    ShapeNetFunctionality: histogram

---
+++ DatasetFunctionality.py
@@ -0,0 +1,23 @@
+import torch
+from torch_geometric.data import Data
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class ShapeNetFunctionality:
+
+    @staticmethod
+    def histogram(dataset, name):
+        nb_nodes = ShapeNetFunctionality.get_number_of_nodes(dataset)
+        plt.hist(nb_nodes)
+        plt.title("Nb Points: " + name)
+        plt.show()
+
+    @staticmethod
+    def get_number_of_nodes(dataset):
+        nb_nodes = np.zeros(len(dataset), dtype=int)
+        for i in range(len(dataset)):
+            nb_nodes[i] = dataset[i].num_nodes
+
+        return nb_nodes
+
